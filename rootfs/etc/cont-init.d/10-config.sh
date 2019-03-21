72b4718b155028c89c9905688dbdf29184d969e8
Jason Kulatunga <jason@thesparktree.com>
2019-03-20 20:38:00 -0700

    trying to setup modifiable UID and GID

--- rootfs/etc/cont-init.d/10-config.sh
+++ rootfs/etc/cont-init.d/10-config.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/with-contenv bash
 
+PUID=${PUID:-15000}
+PGID=${PGID:-15000}
 
 # - Documents created by www-data user, not 15000 like all other containers.
 # change mayan userid
-#usermod -u 15000 mayan
-#groupmod -g 15000 mayan
-#
-#chown -R mayan:mayan /opt/mayan-edms
+groupmod -g "$PGID" mayan
+usermod -u "$PUID" mayan
+
+chown -R mayan:mayan "$PROJECT_INSTALL_DIR"
 
 # fix for http://mayan-edms.1003.x6.nabble.com/Mayan-EDMS-2001-docker-local-py-settings-import-error-td5002909.html
 # - Startup /var/lib/mayan/settings folder needs to exist for local.py settings to be created
 mkdir -p /var/lib/mayan/settings
 
-chown -R mayan:mayan /var/lib/mayan/settings
+chown -R mayan:mayan /var/lib/mayan
 
 
 # - FUSE index/volume mount - https://docs.mayan-edms.com/chapters/indexes.html?highlight=fuse
